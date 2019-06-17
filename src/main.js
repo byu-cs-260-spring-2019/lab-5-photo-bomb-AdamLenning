b40177a89a07df6a18619b917291a50b120ba1d3
Adam Lenning <adam.lenning.14@gmail.com>
2019-06-17 12:15:13 -0600

    Added the entire project ;)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,27 @@ new Vue({
   store,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+
+Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAYBJubC3PUzVjJPcWsvzA3dsmaab7mUVg",
+    authDomain: "authproject-31270.firebaseapp.com",
+    databaseURL: "https://authproject-31270.firebaseio.com",
+    projectId: "authproject-31270",
+    storageBucket: "authproject-31270.appspot.com",
+    messagingSenderId: "567846446386",
+    appId: "1:567846446386:web:3b1adbc403c29d81"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(user => {
+  if(user){
+    store.commit('setUser',user);
+  }
+  else {
+    store.commit('setUser',null);
+  }
+});
